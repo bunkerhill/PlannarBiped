7d7943f07e1241fd095c694f9a2f16393a173467
Chunchu <80113415+ChunchuZhu@users.noreply.github.com>
2023-09-14 22:51:51 -0400

    get it down

--- ACC2024/Lip_decouple/walk_simulate1.m
+++ ACC2024/Lip_decouple/walk_simulate1.m
@@ -45,19 +45,32 @@ for i=2:length(T)
     fprintf("%d\n",i);
     current_T = T(i-1);
     if i >= 30 && i <= 130
-        ddxy_s = [0.56;0]; % 0.56  0.58
+        ddxy_s = [0.57;0]; % 0.56  0.58
     else
         ddxy_s = [0;0];
     end
 
-    if i == 60
+    if i == 46
         X_z_dot = MPC_controller2.MPC_horizon(x_tank(:,i-1),x_z,ddxy_s,current_T);
+        x_z_normal = x_z;
         for j = 1:(length(X_z_dot)/3)
-            x_z = x_z + dT * x_z_dot;
+            x_z_normal = x_z_normal + dT * X_z_dot(j,:)';
+            normal_u = [normal_u,x_z_normal];
+        end
+        x_z_up = x_z;
+        for j = 1:(length(X_z_dot)/3)
+            x_z_up = x_z_up + dT * X_z_dot(j+(length(X_z_dot)/3),:)';
+            up_u = [up_u,x_z_up];
+        end
+        x_z_low = x_z;
+        for j = 1:(length(X_z_dot)/3)
+            x_z_low = x_z_low + dT * X_z_dot(j+2*(length(X_z_dot)/3),:)';
+            low_u = [low_u,x_z_low];
         end
 
         break
     end
+
 
     x_z_dot = MPC_controller2.MPC(x_tank(:,i-1),x_z,ddxy_s,current_T);
     x_z = x_z + dT * x_z_dot;
@@ -70,11 +83,13 @@ for i=2:length(T)
     ddxy_s_tank = [ddxy_s_tank,ddxy_s];
     dxy_s_tank = [dxy_s_tank,dxy_s];
     xy_s_tank = [xy_s_tank,xy_s];
+
+    
 end
 
 %% plot result
-[X_min_next,X_max_next] = MPC_controller2.get_ZMP_rangex(T_s+dT);
-[Y_min_next,Y_max_next] = MPC_controller2.get_ZMP_rangey(T_s+dT);
+% [X_min_next,X_max_next] = MPC_controller2.get_ZMP_rangex(T_s+dT);
+% [Y_min_next,Y_max_next] = MPC_controller2.get_ZMP_rangey(T_s+dT);
 % figure
 % plot(T,x_tank(2,:))
 % hold on
@@ -108,27 +123,27 @@ end
 % xlabel('t (s)') 
 % ylabel('a (m/s^2)') 
 
-figure
-plot(T,x_tank(1,:))
-hold on
-plot(T,u_tank(1,:))
-plot(T+dT,X_min_next)
-plot(T+dT,X_max_next)
-title('com and zmp postion in x direction')
-xlabel('t (s)') 
-ylabel('position (m)') 
-legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
-
-figure
-plot(T,x_tank(3,:))
-hold on
-plot(T,u_tank(2,:))
-plot(T+dT,Y_min_next)
-plot(T+dT,Y_max_next)
-title('com, zmp and zmp constraint postion in y direction')
-xlabel('t (s)') 
-ylabel('position (m)') 
-legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
+% figure
+% plot(T,x_tank(1,:))
+% hold on
+% plot(T,u_tank(1,:))
+% plot(T+dT,X_min_next)
+% plot(T+dT,X_max_next)
+% title('com and zmp postion in x direction')
+% xlabel('t (s)') 
+% ylabel('position (m)') 
+% legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
+% 
+% figure
+% plot(T,x_tank(3,:))
+% hold on
+% plot(T,u_tank(2,:))
+% plot(T+dT,Y_min_next)
+% plot(T+dT,Y_max_next)
+% title('com, zmp and zmp constraint postion in y direction')
+% xlabel('t (s)') 
+% ylabel('position (m)') 
+% legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
 % 
 % figure
 % i = 200;
@@ -144,20 +159,48 @@ for i=1:length(X_min_next)
     hold on
 end
 
-i = length(X_min_next);
-plot(u_tank(1,1:i),u_tank(2,1:i))
+% connect
+u_tank = [u_tank, normal_u(:,1)];
 
-plot(x_tank(1,1:i),x_tank(3,1:i))
+plot(u_tank(1,:),u_tank(2,:))
+plot(normal_u(1,:),normal_u(2,:))
+
+plot(up_u(1,:),up_u(2,:))
+plot(low_u(1,:),low_u(2,:))
+
+
+% plot(x_tank(1,1:i),x_tank(3,1:i))
 
 % plot(X_min_next,Y_min_next)
 % plot(X_max_next,Y_max_next)
 
 
-title('COM trajectory and foot position trajectory')
+title('foot position trajectory')
 xlabel('x (m)') 
 ylabel('y (m)') 
-legend({'foot position','COM'},'Location','southwest')
+legend({'exist_COM','normal_COM','up_COM','low_COM'},'Location','southwest')
 axis equal 
+
+%%
+figure
+plot(normal_u(1,:))
+hold on
+plot(up_u(1,:))
+plot(low_u(1,:))
+title('com, zmp and zmp constraint postion in y direction')
+xlabel('t (s)') 
+ylabel('position (m)') 
+legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
+
+figure
+plot(normal_u(2,:))
+hold on
+plot(up_u(2,:))
+plot(low_u(2,:))
+title('com, zmp and zmp constraint postion in y direction')
+xlabel('t (s)') 
+ylabel('position (m)') 
+legend({'COM','ZMP','ZMP_lowConstraint','ZMP_upConstraint'})
 
 %% dynamic equation
 function X_next = lip_dynamics(X,u,ddxy_s,dT,L,g)
